fdeee20f7dd003340342bb3e5b0237449b79709b
xNoFear <kevin-engelhardt@gmx.de>
2018-11-20 21:01:43 +0100

    cv b vb hb hn

---
+++ src/main/java/PrimzahlService.java
@@ -0,0 +1,32 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+
+@Path (PrimzahlService.webContextPath)
+
+
+public class PrimzahlService {
+
+
+    {
+        static final String webContextPath = "/primzahl";
+
+        @GET
+        @Produces( MediaType.TEXT_PLAIN )
+        public String halloPlainText( @QueryParam("name") String name )
+        {
+            return "Plain-Text: Hallo " + name;
+        }
+
+        @GET @Produces( MediaType.TEXT_HTML )
+        public String halloHtml( @QueryParam("name") String name )
+        {
+            return "<html><title>HelloWorld</title><body><h2>Html: Hallo " + name + "</h2></body></html>";
+        }
+    }
+
+
+}
---
+++ src/main/java/WriteJson.java
@@ -0,0 +1,11 @@
+import jdk.nashorn.internal.parser.JSONParser;
+
+public class WriteJson {
+
+    public
+
+    public static void main (String[] args)
+    {
+            
+    }
+}
