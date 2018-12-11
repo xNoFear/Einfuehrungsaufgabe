07432548f258a6a00df00968b219b83346fbf723
xNoFear <kevin-engelhardt@gmx.de>
2018-12-11 22:14:33 +0100

    Kommentare

--- src/main/java/PrimzahlService.java
+++ src/main/java/PrimzahlService.java
@@ -12,12 +12,14 @@ import javax.ws.rs.core.Response;
 @Path (PrimzahlService.webContextPath)
 public class PrimzahlService {
 
-
+    //Endpoint der Rest-Anfrage
     static final String webContextPath = "/primzahl";
-
+    //HTTP Verb
     @GET
+    //Rueckgabe als Json Format
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getPrims(@QueryParam("parameter") int parameterN , @QueryParam("returnForm") int returnForm) {
+    public Response getPrims(@QueryParam("parameter") int parameterN , @QueryParam("returnForm") int returnForm) //Query Param liest Inhalt der Uri Anfrage aus
+     {
         System.out.println("Request" +"Count prims: " + parameterN + " " + "ReturnForm: " + returnForm);
 
 
@@ -29,11 +31,11 @@ public class PrimzahlService {
                 return resultAsIntArray(parameterN);
 
 
-            case 3:
+            case 3://Datenstruktur
                 return resultAsDatastructure(parameterN);
 
 
-            default:
+            default://Falls keine der 3 cases eingegeben wird, wird im json Format eine Fehlermeldung zurueckgegeben
                 JSONArray result = new JSONArray();
                 result.put("Result");
                 JSONObject jsonObject = new JSONObject().put("Message", "Invalid Parameter");
@@ -45,10 +47,13 @@ public class PrimzahlService {
 
 
 
+    //Es wird eine Anfrage auf "Ergebnis als String" gestellt
     private Response resultAsString(int numberOfPrims) {
+        //Methode getPrimAsString wird aufgerufen
         String resultAsString = getPrimAsString(numberOfPrims);
         JSONArray result = new JSONArray();
         result.put("Result");
+        //Inhalt der oberen Methode wird in ein Json Array geschrieben
         JSONObject jsonObject = new JSONObject().put("String", resultAsString);
         result.put(jsonObject);
 
@@ -57,36 +62,38 @@ public class PrimzahlService {
     }
 
 
-
+    //Es wird eine Anfrage auf "Ergebnis als Int-Array" gestellt
     private Response resultAsIntArray(int numberOfPrims){
+        //Methode PrimAsIntArray wird aufgerufen
         int [] primsInIntArray = getPrimAsIntArray(numberOfPrims);
         JSONArray result = new JSONArray();
         result.put("Result");
+        //Inhalt der oberen Methode wird in ein Json Array geschrieben
         JSONObject jsonObject = new JSONObject().put("Integer Array", primsInIntArray);
         result.put(jsonObject);
 
         return Response.status(200).entity(result.toString()).build();
     }
 
+    //Es wird eine Anfrage auf "Ergebnis als Datenstruktur" gestellt
     private Response resultAsDatastructure(int numberOfPrims){
+        //Beide Methoden zur Erzeugung der Datenstruktur werden aufgerufen
         String resultAsString = getPrimAsString(numberOfPrims);
         int[] resultAsArray = getPrimAsIntArray(numberOfPrims);
 
-
         JSONArray result = new JSONArray();
         result.put("Result");
-
+        //Inhalt der oberen Methode getPrimAsString wird in ein Json Array geschrieben
         JSONObject jsonString = new JSONObject().put("String", resultAsString);
         result.put(jsonString);
-
+        //Inhalt der Methode getPrimAsIntArray wird in ein Json Array geschrieben
         JSONObject jsonIntArray = new JSONObject().put("IntArray", resultAsArray);
         result.put(jsonIntArray);
-
-
+        //Der Anfrage an den Server wird der Status 200 = "Alles in Ordnung" uebergeben, Json Object wird in einen String umgewandelt und Response wird generiert
         return Response.status(200).entity(result.toString()).build();
     }
 
-
+    //Es wird geprueft, ob die eingegebene Zahl bereits abgefragt wurde, sollte dies der Fall sein, wird keine neue Berechnung durchgefuehrt
     private String getPrimAsString(int numberOfPrims) {
         if (CacheService.isInCacheString(numberOfPrims)) {
             return CacheService.getPrimzahlenAsString(numberOfPrims);
@@ -99,7 +106,7 @@ public class PrimzahlService {
             return resultAsString;
         }
     }
-
+    //Es wird geprueft, ob die eingegebene Zahl bereits abgefragt wurde, sollte dies der Fall sein, wird keine neue Berechnung durchgefuehrt
     private int[] getPrimAsIntArray(int numberOfPrims){
         if (CacheService.isInCacheIntArray(numberOfPrims)) {
             return CacheService.getPrimsAsIntArray(numberOfPrims);
